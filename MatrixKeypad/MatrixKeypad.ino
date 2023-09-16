f2b96c738316cd10b891a3e7bbf9317a7aa07a23
MadhanM123 <madhangopalvel@gmail.com>
2023-09-15 17:31:10 -0700

    matrix based keypad

---
+++ MatrixKeypad/MatrixKeypad.ino
@@ -0,0 +1,31 @@
+#include <Keypad.h>
+
+const byte rows = 4;
+const byte cols = 4;
+
+char KEYS[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte ROW_PINS[rows] = {9, 8, 7, 6};
+byte COL_PINS[cols] = {5, 4, 3, 2};
+
+Keypad myPad = Keypad(makeKeymap(KEYS), ROW_PINS, COL_PINS, rows, cols);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char pressed = myPad.getKey();
+
+  if(pressed){
+    Serial.println(pressed);
+    if(pressed == '5'){
+      Serial.println("Wow");
+    }
+  }
+}
